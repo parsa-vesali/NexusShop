37fa5f6d9806b67e20c4e30a72910010970f4c81
Parsa Vesali <parsavesali84@gmail.com>
2024-01-15 20:36:31 +0330

    add introduction

--- Script/app.js
+++ Script/app.js
@@ -1,3 +1,4 @@
+// Selecting elements
 const submenuOpenBtn = document.querySelector(".mobile-menu__submenu-icon");
 const submenu = document.querySelector(".mobile-menu__submenu");
 const navOpenBtn = document.querySelector(".mobile-menu__open-icon");
@@ -9,36 +10,59 @@ const cartCloseBtn = document.querySelector(".mobile-cart__close");
 const cart = document.querySelector(".mobile-cart");
 const openCartBtn = document.querySelector(".open-cart__btn");
 
-
+// Event listener for submenu toggle
 submenuOpenBtn.addEventListener("click", (e) => {
     submenu.classList.toggle('submenu-active')
-})
+});
 
-
+// Function to close navigation and submenu
 function closeNav() {
-    nav.classList.remove('active-menu')
-    submenu.classList.remove('submenu-active')
+    nav.classList.remove('active-menu');
+    submenu.classList.remove('submenu-active');
+    overlay.classList.remove('overlay-active');
 }
+
+// Function to close the cart
 function closeCart() {
     cart.classList.remove("active-cart");
+    overlay.classList.remove('overlay-active');
 }
-openCartBtn.addEventListener('click' , ()=> {
+
+// Event listener for opening the cart
+openCartBtn.addEventListener('click', () => {
     cart.classList.add("active-cart");
-    nav.classList.remove('active-menu')
-})
+    nav.classList.remove('active-menu');
+});
 
-
+// Event listener for opening the navigation
 navOpenBtn.addEventListener("click", () => {
-    nav.classList.add('active-menu')
+    nav.classList.add('active-menu');
     cart.classList.remove("active-cart");
-})
+    overlay.classList.add('overlay-active');
+});
+
+// Event listener for opening the cart
 cartOpenBtn.addEventListener("click", () => {
     cart.classList.add("active-cart");
-    nav.classList.remove('active-menu')
-})
+    nav.classList.remove('active-menu');
+    overlay.classList.add('overlay-active');
+});
 
+// Event listener for closing the navigation
 navCloseBtn.addEventListener("click", closeNav);
+
+// Event listener for closing the cart
 cartCloseBtn.addEventListener("click", closeCart);
+
+// Event listener for overlay click to close navigation and cart
+overlay.addEventListener('click', overlayClose);
+
+// Function to close navigation and cart on overlay click
+function overlayClose() {
+    closeNav();
+    closeCart();
+}
+
 
 
 
@@ -50,9 +74,16 @@ var swiper = new Swiper(".mySwiper", {
         delay: 2500,
         disableOnInteraction: false,
     },
+    loop: true,
     effect: "fade",
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
 });
+
+
+
+
+// ================ AOS ===================
+AOS.init();
